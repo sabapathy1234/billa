7cfe8a6ff2969d94cc998a40296d03efe83c41d1
Selvakumar030998 <52271089+Selvakumar030998@users.noreply.github.com>
2019-06-27 11:35:51 +0530

    Update billa1.py

--- billa1.py
+++ billa1.py
@@ -1,7 +1,7 @@
 num = float(input("Enter the number:"))
 if num > 0:
-	print("{0}is a positive number".format(num))
+	print("positive number")
 elif num == 0:
-	print("{0} is zero ".format(num))
+	print("zero ")
 else:
-	print("{0} is a negative number".format(num))
+	print("negative number")

bc7dc5d94089a279ee89fd98e6cd756851e7d036
Selvakumar030998 <52271089+Selvakumar030998@users.noreply.github.com>
2019-06-28 10:46:58 +0530

    Update billa5.py

--- billa5.py
+++ billa5.py
@@ -1,7 +1,7 @@
-b2,b3,b4=input().split()
-if b3>b4:
-   print(b3)
-elif b2>b3 and b2>b4:
-   print(b2)
-else:
+b3,b4,b5=input().split()
+if b4>b5:
    print(b4)
+elif b3>b4 and b3>b5:
+   print(b3)
+else:
+   print(b5)
